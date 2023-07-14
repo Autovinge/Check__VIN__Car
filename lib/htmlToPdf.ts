5f368f523219a608c291f779cf169dffb68fb80c
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-07-14 20:02:51 +0400

    correct variable name

--- lib/htmlToPdf.ts
+++ lib/htmlToPdf.ts
@@ -17,11 +17,11 @@ const generatePDF = async (html: string) => {
   const data = await inlineCss(html, { url: '/' })
   const template = hb.compile(data, { strict: true })
   const result = template(data)
-  const HTML = result
+  const parsedHTML = result
 
   const browser = await puppeteer.launch({ args: args, headless: 'new' })
   const page = await browser.newPage()
-  await page.setContent(HTML, { waitUntil: 'networkidle0' })
+  await page.setContent(parsedHTML, { waitUntil: 'networkidle0' })
   const pdf = await page.pdf({ format: 'A4' })
   await browser.close()
   return pdf
