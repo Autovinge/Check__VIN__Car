d84a9ea1c62ef5a409fb7da71725d1c36d0d832a
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-07-16 10:43:40 +0400

    create browser instance only on first function call

--- lib/htmlToPdf.ts
+++ lib/htmlToPdf.ts
@@ -2,29 +2,33 @@ import puppeteer from 'puppeteer'
 import inlineCss from 'inline-css'
 import hb from 'handlebars'
 
+const args = [
+  '--no-sandbox',
+  "--disable-setup-sandbox",
+  '--disable-setuid-sandbox',
+  '--disable-dev-shm-usage',
+  '--disable-accelerated-2d-canvas',
+  '--no-first-run',
+  '--no-zygote',
+  '--single-process', // <- this one doesn't works in Windows
+  '--disable-gpu'
+]
+let browser;
+
 const generatePDF = async (html: string) => {
-  let args = [
-    '--no-sandbox',
-    "--disable-setup-sandbox",
-    '--disable-setuid-sandbox',
-    '--disable-dev-shm-usage',
-    '--disable-accelerated-2d-canvas',
-    '--no-first-run',
-    '--no-zygote',
-    '--single-process', // <- this one doesn't works in Windows
-    '--disable-gpu'
-  ]
   const data = await inlineCss(html, { url: '/' })
   const template = hb.compile(data, { strict: true })
   const result = template(data)
   const parsedHTML = result
 
-  const browser = await puppeteer.launch({ args: args, headless: 'new' })
+  if (!browser) browser = await puppeteer.launch({ args: args, headless: 'new' })
+
   const page = await browser.newPage()
   await page.setContent(parsedHTML, { waitUntil: 'networkidle0' })
   const pdf = await page.pdf({ format: 'A4' })
-  await browser.close()
+  await page.close()
   return pdf
+
 }
 
 export default generatePDF
