f555a85150420528cb9a14dc5fa9c3aa4b60775a
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-07-16 13:00:00 +0400

    rename file to generatePDF

--- lib/generatePDF.ts
+++ lib/generatePDF.ts
@@ -1,3 +1,4 @@
+
 import puppeteer from 'puppeteer'
 import inlineCss from 'inline-css'
 import hb from 'handlebars'
@@ -15,13 +16,16 @@ const args = [
 ]
 let browser;
 
+export const startBrowser = async () => {
+  browser = await puppeteer.launch({ args: args, headless: 'new' })
+}
+
 const generatePDF = async (html: string) => {
   const data = await inlineCss(html, { url: '/' })
   const template = hb.compile(data, { strict: true })
   const result = template(data)
   const parsedHTML = result
 
-  if (!browser) browser = await puppeteer.launch({ args: args, headless: 'new' })
 
   const page = await browser.newPage()
   await page.setContent(parsedHTML, { waitUntil: 'networkidle0' })
