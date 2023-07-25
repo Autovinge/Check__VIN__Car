42fc1c953dae01f9bbca72c16f9b02f271f5dbb0
Tornike Shavidze <shavidzet@gmail.com>
2023-07-25 20:13:57 +0000

    refactor utils

--- lib/validate.ts
+++ lib/validate.ts
@@ -5,16 +5,9 @@ const vincodeRegex = new RegExp(
   '^[A-HJ-NPR-Z\\d]{8}[\\dX][A-HJ-NPR-Z\\d]{2}\\d{6}$'
 )
 
-export const validateMail = (mail: string) => {
-  if (mail.match(emailRegex)) return true
-  return false
-}
-export const validateVincode = (vincode: string) => {
-  if (vincode.match(vincodeRegex)) return true
-  return false
-}
+export const validateMail = (mail: string) => Boolean(mail.match(emailRegex))
 
-export const validateVendor = (vendor: string) => {
-  if (vendor === vendors[0] || vendor === vendors[1]) return true
-  return false
-}
+export const validateVincode = (vincode: string) =>
+  Boolean(vincode.match(vincodeRegex))
+
+export const validateVendor = (vendor: string) => vendors.includes(vendor)
