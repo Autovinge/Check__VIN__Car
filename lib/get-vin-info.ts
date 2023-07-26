f81606c71b58fa67c727af7542c19fb73acb49fe
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-07-26 19:29:39 +0400

    format code

--- lib/get-vin-info.ts
+++ lib/get-vin-info.ts
@@ -1,14 +1,20 @@
-const getVinInfo = async (vincode: string, vendor: string, retries:number = 3) => {
+const getVinInfo = async (
+  vincode: string,
+  vendor: string,
+  retries: number = 3
+) => {
   try {
-    const response = await fetch(`${process.env.API_URL}/${vendor}?vincode=${vincode}&api_key=${process.env.API_KEY}`)
+    const response = await fetch(
+      `${process.env.API_URL}/${vendor}?vincode=${vincode}&api_key=${process.env.API_KEY}`
+    )
     const data = await response.json()
     return data
-      } catch (err) {
-      if (retries > 0) {
-        return getVinInfo(vincode, vendor, retries - 1)
-      } else {
-        throw new Error("Maximum retries reached")
-      }
+  } catch (err) {
+    if (retries > 0) {
+      return getVinInfo(vincode, vendor, retries - 1)
+    } else {
+      throw new Error('Maximum retries reached')
+    }
   }
 }
 export default getVinInfo
