e903c7e8c8088e1ddadd076bfaf3a685b5f87c9a
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-07-26 19:34:47 +0400

    move code from fork

---
+++ lib/generatePDF.ts
@@ -0,0 +1,22 @@
+import inlineCss from 'inline-css'
+import hb from 'handlebars'
+import getConfig from 'next/config'
+
+const { serverRuntimeConfig } = getConfig()
+
+const generatePDF = async (html: string) => {
+  const puppeteerInstance = await serverRuntimeConfig.puppeteerInstance()
+  const data = await inlineCss(html, { url: '/' })
+  const template = hb.compile(data, { strict: true })
+  const result = template(data)
+  const parsedHTML = result
+
+  const page = await puppeteerInstance.newPage()
+  await page.setContent(parsedHTML, { waitUntil: 'networkidle0' })
+  const pdf = await page.pdf({ format: 'A4' })
+  await page.close()
+  console.log(pdf)
+  return pdf
+}
+
+export default generatePDF
--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
+const puppeteer = require('puppeteer')
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  serverRuntimeConfig: {
+    puppeteerInstance: async () => {
+      const browser = await puppeteer.launch({
+        headless: 'new',
+        args: [
+          '--no-sandbox',
+          '--disable-setup-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-accelerated-2d-canvas',
+          '--no-first-run',
+          '--no-zygote',
+          '--single-process', // <- this one doesn't works in Windows
+          '--disable-gpu'
+        ]
+      })
+      return browser
+    }
+  }
+}
