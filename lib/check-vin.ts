9e030ef7269818996a2670e78ce470060f4ad0c8
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-07-26 19:27:22 +0400

    move code from fork

---
+++ lib/check-vin.ts
@@ -0,0 +1,15 @@
+const checkVin = async (vincode: string, vendor: string, retries: number = 0) => {
+  try {
+    const response = await fetch(`${process.env.API_URL}/${vendor}/check?vincode=${vincode}&api_key=${process.env.API_KEY}`)
+    const data = await response.json()
+    return data
+  } catch (err) {
+      if (retries > 0) {
+        return checkVin(vincode, vendor, retries - 1)
+      } else {
+        throw new Error("Maximum retries reached")
+      }
+    }
+  }
+
+  export default checkVin
---
+++ lib/get-vin-info.ts
@@ -0,0 +1,14 @@
+const getVinInfo = async (vincode: string, vendor: string, retries:number = 3) => {
+  try {
+    const response = await fetch(`${process.env.API_URL}/${vendor}?vincode=${vincode}&api_key=${process.env.API_KEY}`)
+    const data = await response.json()
+    return data
+      } catch (err) {
+      if (retries > 0) {
+        return getVinInfo(vincode, vendor, retries - 1)
+      } else {
+        throw new Error("Maximum retries reached")
+      }
+  }
+}
+export default getVinInfo
