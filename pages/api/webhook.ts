bb76bae1bba84035c0073f84f060c3928269f901
Goga Imerlishvili <gogaimerlishvili1@gmail.com>
2025-08-11 02:37:59 +0400

    wbhk

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -7,17 +7,17 @@ import {
 import getVinInfo from '../../lib/get-vin-info'
 import generatePDF from '../../lib/generatePDF'
 import { sendMail } from '../../lib/mail'
+
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { PaymentStatus, PaymentId } = req.body
 
     switch (PaymentStatus) {
       case 'Timeout':
-        await deleteDocumentById(PaymentId)
-        res.send({})
       case 'Rejected':
         await deleteDocumentById(PaymentId)
-        res.send({})
+        res.status(200).send({})
+        break
       case 'Captured':
         try {
           const doc = await getDocumentById(PaymentId)
@@ -31,11 +31,17 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
           }
           await sendMail(doc.mail, doc.vincode, doc.vendor, pdfBuffer)
           await updateSentMail(PaymentId)
-          res.send({})
+          res.status(200).send({})
         } catch (err) {
-          res.send({})
+          res.status(200).send({})
         }
+        break
+      default:
+        res.status(200).send({})
+        break
     }
+  } else {
+    res.status(405).json({ msg: 'Method Not Allowed' })
   }
 }
 
