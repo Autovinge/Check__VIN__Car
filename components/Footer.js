70d38b283d99b4442ec7483eb72d48a57f16c635
Goga Imerlishvili <gogaimerlishvili1@gmail.com>
2025-08-11 02:41:08 +0400

    change footer info

--- components/Footer.js
+++ components/Footer.js
@@ -48,26 +48,7 @@ const Footer = () => {
             flexDirection="column"
             h="70px"
           >
-            <Box display="flex" alignItems="space-evenly" h="80%">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="1.5em"
-                viewBox="0 0 496 512"
-                fill="white"
-              >
-                <path d="M248,8C111.033,8,0,119.033,0,256S111.033,504,248,504,496,392.967,496,256,384.967,8,248,8ZM362.952,176.66c-3.732,39.215-19.881,134.378-28.1,178.3-3.476,18.584-10.322,24.816-16.948,25.425-14.4,1.326-25.338-9.517-39.287-18.661-21.827-14.308-34.158-23.215-55.346-37.177-24.485-16.135-8.612-25,5.342-39.5,3.652-3.793,67.107-61.51,68.335-66.746.153-.655.3-3.1-1.154-4.384s-3.59-.849-5.135-.5q-3.283.746-104.608,69.142-14.845,10.194-26.894,9.934c-8.855-.191-25.888-5.006-38.551-9.123-15.531-5.048-27.875-7.717-26.8-16.291q.84-6.7,18.45-13.7,108.446-47.248,144.628-62.3c68.872-28.647,83.183-33.623,92.511-33.789,2.052-.034,6.639.474,9.61,2.885a10.452,10.452,0,0,1,3.53,6.716A43.765,43.765,0,0,1,362.952,176.66Z" />
-              </svg>
-              <Link
-                href="/"
-                fontSize={{ base: '14px', md: '15px', lg: '16px' }}
-                ml="10px"
-                textColor="white"
-              >
-                {' '}
-                @companyname
-              </Link>
-            </Box>
-
+    
             <Box display="flex" alignItems="center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -84,7 +65,29 @@ const Footer = () => {
                 textColor="white"
                 mt="5px"
               >
-                CompanyEmail@gmail.com
+                Autovin.ge@gmail.com
+              </Link>
+            </Box>
+
+            <Box display="flex" alignItems="center" mt="5px">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1.5em"
+                viewBox="0 0 496 512"
+                fill="white"
+              >
+                <path d="M248 8C111 8 0 119 0 256c0 137 111 248 248 248s248-111 248-248C496 119 385 8 248 8zm104.6 148.5l-36.7 173.7c-2.8 12.6-10.2 15.7-20.6 9.8l-57-42.1-27.5 26.5c-3 3-5.5 5.5-11.2 5.5l4-56.6 103.1-93c4.5-4-1-6.2-7-2.2l-127.6 80.2-55-17.2c-12-3.7-12.2-12-2.5-15.8l215.2-83.1c9.9-3.7 18.6 2.4 15.1 15.7z" />
+              </svg>
+              <Link
+                href="https://t.me/autovinge"
+                isExternal
+                fontSize={{ base: '14px', md: '15px', lg: '16px' }}
+                ml="10px"
+                textColor="white"
+                mt="2px"
+                _hover={{ textDecoration: 'underline', color: 'teal.200' }}
+              >
+                Telegram: @autovinge
               </Link>
             </Box>
           </Box>
