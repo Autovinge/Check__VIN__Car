b64ad6b18afe88762e75a210caebf81dbfe4dbf1
Goga Imerlishvili <gogaimerlishvili1@gmail.com>
2025-08-11 02:25:15 +0400

    value finder

--- lib/getEnvVar.ts
+++ lib/getEnvVar.ts
@@ -1,12 +1,6 @@
 export function getEnvVar(key: string, defaultValue?: string): string {
   const value = process.env[key]
-
-  if (value === undefined) {
-    if (defaultValue !== undefined) {
-      return defaultValue
-    }
-    throw new Error(`${key} not found in process.env`)
-  }
-
-  return value
+  if (value !== undefined) return String(value)
+  if (defaultValue !== undefined) return defaultValue
+  throw new Error(`${key} not found in process.env`)
 }
